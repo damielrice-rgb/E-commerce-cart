6621366c3d1c16f67d29de3b96954c210c09d538
damiel <damielrice@gmail.com>
2026-01-24 09:10:20 +0700

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -117,9 +117,12 @@ object3.method();
 });
 console.log('load products'); 
 
+    }).catch((error) => {
+      console.log('Unexpected error. Please try again later');
     });
     return promise;
   }
+  
   /*
   loadProductsFetch().then(() => {
     console.log('next step');
@@ -141,9 +144,14 @@ console.log('load products');
 fun();
     });
 
+    xhr.addEventListener('error', (error) => {
+      console.log('Unexpected error. Please try again later');
+    });
+
     xhr.open('GET', 'https://supersimplebackend.dev/products');
     xhr.send();
   }
+  
   
 
 
